5ed51927b33cec02d16428a5a493a997d3ef2bdf
Savannah Israel <sisrael0405@gmail.com>
2019-05-11 19:26:27 -0600

    added basic HTML and JS psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Global Variables:
+    targetScore
+    //yourScore
+    //winCount
+    //lossCount
+    //crystal1
+    //crysatl2
+    //crystal3
+    //crystal4
+    
+
+
+//Methods/Logic
+    
+    //function to generate random number - for each crystal and for target score
+        //array of crystals loop through, assign a value for each crystal
+
+
+    //click event for crystals - 
+        // *assign all crystals with same class so you only have to write one click event
+   
+   
+    //start/reset game function
+        //clear out variables
